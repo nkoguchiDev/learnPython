d409da7c8d89de427417feda16193405808c96f3
nkoguchi <nkoguchi00dev@gmail.com>
2022-06-09 01:42:02 +0900

    WIP

--- life_simulation/app/tests/nodes/test_common.py
+++ life_simulation/app/tests/nodes/test_common.py
@@ -1,10 +1,24 @@
 from app.nodes.common import common
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    life: int = 0
+    alive: bool = True
+    x: int = 0
+    y: int = 0
 
 
 class TestCommon:
     def test_move_and_stop(self):
-        node = common()
+        node = common(Node())
+        life = node.life
 
-        for i in range(10):
+        for i in range(life - 1):
             node.move()
+            print(node.life, node.x, node.y)
+        assert node.alive is True
+
+        node.move()
         assert node.alive is False
--- life_simulation/app/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from os import lseek
-
-from app.main import main
-
-
-def test_main():
-    assert main() == 0
